1a5a52726f2abfc8aa9f1c4606380152bbe1a1a0
spiered <61991144+spiered@users.noreply.github.com>
2020-10-21 16:06:18 +0200

    Bootsrap

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const geoip = require('geoip-lite');
 const ip = "207.97.227.239";
 const geo = geoip.lookup(ip);
 
-console.log(geo);
+let ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
